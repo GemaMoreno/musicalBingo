b40496802062a9c30c7191948c8b573f6b5a3556
Gema <gmc35@alu.ua.es>
2025-08-16 03:25:27 +0200

    First version musical bingo card generator

---
+++ songs.js
@@ -0,0 +1,23 @@
+// Lista de canciones de tu playlist
+const songs = [
+    "Canción 1",
+    "Canción 2",
+    "Canción 3",
+    "Canción 4",
+    "Canción 5",
+    "Canción 6",
+    "Canción 7",
+    "Canción 8",
+    "Canción 9",
+    "Canción 10",
+    "Canción 11",
+    "Canción 12",
+    "Canción 13",
+    "Canción 14",
+    "Canción 15",
+    "Canción 16",
+    "Canción 17",
+    "Canción 18",
+    "Canción 19",
+    "Canción 20"
+];
